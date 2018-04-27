b58fe8143e947bcf3d506dadd1bb37addab2f729
chien tran thien <vinadonquixote@gmail.com>
2018-04-27 22:01:57 +0700

    added stop list

--- src/main/java/com/company/search/analysis/Analysis.java
+++ src/main/java/com/company/search/analysis/Analysis.java
@@ -1,4 +1,9 @@
 package com.company.search.analysis;
 
 public class Analysis {
+
+    public static final String[] STOP_LIST = new String[]{
+            "a", "an", "and", "are", "as", "at", "be", "by", "for", "from", "has", "he", "in",
+            "is", "it", "its", "of", "on", "that", "the", "to", "was", "were", " will", "with"
+    };
 }
