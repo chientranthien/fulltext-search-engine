d6a7816b126ca207821577dd4820b234daf0b869
chien tran thien <vinadonquixote@gmail.com>
2018-05-01 20:39:34 +0700

    coded lowercaseFilterTests

---
+++ src/test/java/com/company/search/analysis/filter/LowercaseFilterTests.java
@@ -0,0 +1,25 @@
+package com.company.search.analysis.filter;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class LowercaseFilterTests {
+
+    TokenFilter tokenFilter = new LowercaseFilter();
+
+    @Test
+    public void test1() {
+        String[] inputs = new String[]{"By", "SENDING", "CHILDREN", "To", "Their", "ROOMS", "AS", "PUNISHMENT",
+                "We", "teaCH", "thEM", "thE", "IDEa", "THAt", "ALONENESS", "is", "a", "PRIVATion"};
+
+        String[] expectations = new String[]{"by", "sending", "children", "to", "their", "rooms", "as", "punishment",
+                "we", "teach", "them", "the", "idea", "that", "aloneness", "is", "a", "privation"};
+
+        String[] filteredTokens = tokenFilter.filter(inputs);
+
+        Assertions.assertThat(filteredTokens).isEqualTo(expectations);
+
+    }
+
+
+}
