19548206fd710458528e9c2351d67d1be827cfd9
chien tran thien <vinadonquixote@gmail.com>
2018-06-17 14:10:37 +0700

    coded indexWriterImpl

--- src/main/java/com/chientt/search/storage/writer/IndexWriterImpl.java
+++ src/main/java/com/chientt/search/storage/writer/IndexWriterImpl.java
@@ -1,10 +1,11 @@
 package com.chientt.search.storage.writer;
 
 import com.chientt.search.storage.entity.IndexData;
+import com.chientt.search.storage.util.DataUtils;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Map;
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.*;
 
 
 public class IndexWriterImpl implements IndexWriter {
@@ -15,12 +16,39 @@ public class IndexWriterImpl implements IndexWriter {
     }
 
     public long write(IndexData indexData) {
-        Path path = Paths.get(pathStr);
-
-        return 0;
+        long offset = 0;
+        String encodedIndexData = DataUtils.encodeIndexData(indexData);
+        try {
+            Path path = Paths.get(pathStr);
+            if (!Files.exists(path)) {
+                Files.createFile(path);
+            }
+            offset = Files.size(path);
+            Files.write(path,
+                    Arrays.asList(encodedIndexData),
+                    StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return offset;
     }
 
     public long write(Map<String, IndexData> allIndexData) {
-        return 0;
+        long offset = 0;
+
+        Collection<IndexData> indexDataCollection = allIndexData.values();
+        List<String> lines = new ArrayList<>();
+        for (IndexData indexData : indexDataCollection) {
+            String encodeIndexData = DataUtils.encodeIndexData(indexData);
+            lines.add(encodeIndexData);
+        }
+        try {
+            Path path = Paths.get(pathStr);
+            offset = Files.size(path);
+            Files.write(path, lines);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return offset;
     }
 }
