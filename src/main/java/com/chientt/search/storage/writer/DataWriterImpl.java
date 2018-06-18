bfcae2fa31c983608c73068f4765822c0b8b8951
Chien <1112026@student.hcmus.edu.vn>
2018-06-18 15:06:11 +0700

    coded dataWriter

--- src/main/java/com/chientt/search/storage/writer/DataWriterImpl.java
+++ src/main/java/com/chientt/search/storage/writer/DataWriterImpl.java
@@ -1,6 +1,12 @@
 package com.chientt.search.storage.writer;
 
 import com.chientt.search.storage.entity.Data;
+import com.chientt.search.storage.util.DataUtils;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 /**
  *
@@ -8,9 +14,27 @@ import com.chientt.search.storage.entity.Data;
  */
 public class DataWriterImpl implements DataWriter {
 
+    private String pathStr;
+
+    public DataWriterImpl(String pathStr) {
+        this.pathStr = pathStr;
+    }
+
     @Override
     public long write(Data data) {
-        
+        long offset = 0;
+        Path path = Paths.get(pathStr);
+        try {
+            if (!Files.exists(path)) {
+                Files.createFile(path);
+            }
+            offset = Files.size(path);
+            String encodedData = DataUtils.encodeData(data);
+            Files.write(path, encodedData.getBytes(), StandardOpenOption.APPEND);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return offset;
     }
 
 }
